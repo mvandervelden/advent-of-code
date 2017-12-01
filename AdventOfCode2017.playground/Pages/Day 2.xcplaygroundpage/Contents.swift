9384c99ccaa4555e9ef4a4db4cc21653d5d7ee60
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2017-12-01 09:53:35 +0100

    prepared day 2

--- AdventOfCode2017.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
+++ AdventOfCode2017.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
@@ -2,6 +2,28 @@
 
 import Foundation
 
-var str = "Hello, playground"
+class Reader {
+    func read1(_ fileName: String = "input") -> [Int] {
+        let fileURL = Bundle.main.url(forResource: fileName, withExtension: "txt")
+        let content = try! String(contentsOf: fileURL!)
+        return content.flatMap {
+            Int(String($0))
+        }
+    }
+}
 
+func solve1() {
+    let ints = Reader().read1()
+
+    print(ints)
+}
+
+func solve2() {
+    let ints = Reader().read1()
+
+    print(ints)
+}
+
+solve1()
+//solve2()
 //: [Next](@next)
