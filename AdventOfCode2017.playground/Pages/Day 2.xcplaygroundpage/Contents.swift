48fe2c7000d7f32ccad498e58d935fb10d85aa46
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2017-12-02 10:38:40 +0100

    solved part 1 day 2

--- AdventOfCode2017.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
+++ AdventOfCode2017.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
@@ -3,27 +3,32 @@
 import Foundation
 
 class Reader {
-    func read1(_ fileName: String = "input") -> [Int] {
+    func read1(_ fileName: String = "input") -> [[Int]] {
         let fileURL = Bundle.main.url(forResource: fileName, withExtension: "txt")
         let content = try! String(contentsOf: fileURL!)
-        return content.flatMap {
-            Int(String($0))
+        let lines = content.split(separator: "\n")
+        return lines.map { line in
+            line.split(separator: "\t").map {
+                let string = String($0).trimmingCharacters(in: .whitespacesAndNewlines)
+                return Int(string)! }
         }
     }
 }
 
 func solve1() {
-    let ints = Reader().read1()
-
-    print(ints)
+    let sheet = Reader().read1()
+    let checksum = sheet.reduce(0) { (sum, line) in
+        return sum + (line.max()! - line.min()!)
+    }
+    print(checksum)
 }
 
 func solve2() {
-    let ints = Reader().read1()
+    let sheet = Reader().read1()
 
-    print(ints)
+    print(sheet)
 }
 
-solve1()
-//solve2()
+//solve1()
+solve2()
 //: [Next](@next)
