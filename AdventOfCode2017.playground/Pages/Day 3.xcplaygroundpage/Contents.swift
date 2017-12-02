4b7472d4938177c80f21a1c4bd79172111c12ffd
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2017-12-02 11:38:09 +0100

    2017 prepared day 3

--- AdventOfCode2017.playground/Pages/Day 3.xcplaygroundpage/Contents.swift
+++ AdventOfCode2017.playground/Pages/Day 3.xcplaygroundpage/Contents.swift
@@ -2,6 +2,29 @@
 
 import Foundation
 
-var str = "Hello, playground"
+func read1(_ fileName: String = "input") -> [[Int]] {
+    let fileURL = Bundle.main.url(forResource: fileName, withExtension: "txt")
+    let content = try! String(contentsOf: fileURL!)
+    let lines = content.split(separator: "\n")
+    return lines.map { line in
+        line.split(separator: "\t").map {
+            let string = String($0).trimmingCharacters(in: .whitespacesAndNewlines)
+            return Int(string)! }
+    }
+}
+func solve1() {
+    let ints = read1()
 
+    var sum = 0
+    print(sum)
+}
+
+func solve2() {
+    let ints = read1()
+
+    print(ints)
+}
+
+solve1()
+//solve2()
 //: [Next](@next)
