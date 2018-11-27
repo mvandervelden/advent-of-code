05963b7cd1cb200b581411df98f61f60275404ab
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2018-11-27 17:12:18 +0100

    Improved and aligned swift and objc examples

--- 2018/lib/objc/example.m
+++ 2018/lib/objc/example.m
@@ -1,11 +1,23 @@
 #import <Foundation/Foundation.h>
 
+// Running:
+// $ gcc -framework Foundation example.m -o example
+// $ ./example [inputfilename]
+// If no argument provided, it takes "input.txt"
+
+
 @interface Solver: NSObject
-- (NSString *)solve: (NSString *)inputFileName;
+- (NSString *)solve:(NSString *)inputFileName;
 @end
 
 @implementation Solver
-- (NSString *)solve: (NSString *)inputFileName {
+- (NSString *)solve:(NSString *)inputFileName {
+    NSString *input = [self readFile:inputFileName];
+    //TODO solve
+    return input;
+}
+
+- (NSString *)readFile:(NSString *)inputFileName {
     NSURL *currentDirectoryURL = [NSURL fileURLWithPath: NSFileManager.defaultManager.currentDirectoryPath];
     NSURL *url = [[NSURL alloc] initWithString: inputFileName relativeToURL: currentDirectoryURL];
     return [NSString stringWithContentsOfURL: url encoding: NSASCIIStringEncoding error: nil];
@@ -14,10 +26,18 @@
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    NSArray *arguments = [[NSProcessInfo processInfo] arguments];
 
-    NSString *filename = @"input.txt";
+    NSString *filename;
+    if (arguments.count > 1) {
+        filename = arguments[1];
+    } else {
+        filename = @"input.txt";
+    }
+
     Solver *solver = [Solver new];
-    NSLog(@"%@", [solver solve:filename]);
+    NSString *result = [solver solve:filename];
+    NSLog(@"%@", result);
 
     [pool drain];
     return 0;
--- 2018/lib/swift/example.swift
+++ 2018/lib/swift/example.swift
@@ -1,9 +1,35 @@
-func readTerminal(_ fileName: String) -> String {
-    let currentDirectoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
-    guard let fileURL = URL(string: fileName, relativeTo: currentDirectoryURL) else {
-        print("file not found: \(currentDirectoryURL.path)/\(fileName)")
-        return ""
+import Foundation
+
+// Running:
+// $ swift example.swift [inputfilename]
+// If no argument provided, it takes "input.txt"
+
+class Solver {
+
+    func solve(_ fileName: String = "input.txt") -> String {
+        let input = readFile(fileName)
+        //TODO solve
+        return input
     }
-    let content = try! String(contentsOf: fileURL)
-    return content
+
+    private func readFile(_ fileName: String) -> String {
+        let currentDirectoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+        guard let fileURL = URL(string: fileName, relativeTo: currentDirectoryURL) else {
+            print("file not found: \(currentDirectoryURL.path)/\(fileName)")
+            return ""
+        }
+        let content = try! String(contentsOf: fileURL)
+        return content
+    }
 }
+
+let solver = Solver()
+let result: String
+
+if CommandLine.arguments.count > 1 {
+    result = solver.solve(CommandLine.arguments[1])
+} else {
+    result = solver.solve()
+}
+
+print(result)
