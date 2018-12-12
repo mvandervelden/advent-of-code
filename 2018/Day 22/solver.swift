d7b78334c6759908cec08202cba239864f390929
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2018-12-12 20:07:37 +0100

    Added boilerplate for each day

---
+++ 2018/Day 22/solver.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+// Running:
+// $ swift solver.swift [inputfilename]
+// If no argument provided, it takes "input.txt"
+
+class Solver {
+
+    func solve(_ fileName: String = "input.txt") -> String {
+        let input = readFile(fileName)
+        let result1 = solve1(input: input)
+        let result2 = solve2(input: input)
+        return "r1: \(result1)\nr2: \(result2)"
+    }
+
+    private func solve1(input: String) -> String {
+        let lines = input.split(separator: "\n")
+        
+        return ""
+    }
+
+    private func solve2(input: String) -> String {
+        let lines = input.split(separator: "\n")
+
+        return ""
+    }
+
+    private func readFile(_ fileName: String) -> String {
+        let currentDirectoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+        guard let fileURL = URL(string: fileName, relativeTo: currentDirectoryURL) else {
+            print("file not found: \(currentDirectoryURL.path)/\(fileName)")
+            return ""
+        }
+        let content = try! String(contentsOf: fileURL)
+        return content
+    }
+}
+
+let solver = Solver()
+let result: String
+
+if CommandLine.arguments.count > 1 {
+    result = solver.solve(CommandLine.arguments[1])
+} else {
+    result = solver.solve()
+}
+
+print(result)
