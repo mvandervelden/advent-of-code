5190d66631023a81d292afb677ea1708110079cd
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2018-12-22 12:43:49 +0100

    Day 22

--- 2018/Day 22/solver.swift
+++ 2018/Day 22/solver.swift
@@ -91,14 +91,26 @@ class Solver {
         }
     }
 
-    enum Equipment: Hashable {
+    enum Equipment: Hashable, CustomStringConvertible {
         case torch, climbing, neither
+
+        var description: String {
+            switch self {
+                case .torch: return "🔥"
+                case .climbing: return "🧗"
+                case .neither: return "∅"
+            }
+        }
     }
 
-    struct Location: Hashable {
+    struct Location: Hashable, CustomStringConvertible {
         let x: Int
         let y: Int
         let equipment: Equipment
+
+        var description: String {
+            return "(\(x),\(y)) \(equipment)"
+        }
     }
 
     var depth = 7305
@@ -133,7 +145,8 @@ class Solver {
 
     private func solve2() -> String {
         exploreTo(targetX: target.x, targetY: target.y)
-
+        let route = astar(start: Location(x: 0, y: 0, equipment: .torch), goal: Location(x: target.x, y: target.y, equipment: .torch))
+        print(route)
         return ""
     }
 
@@ -147,25 +160,37 @@ class Solver {
     }
 
     func exploreTo(targetX: Int, targetY: Int) {
-        if cave.count >= targetY && cave[targetY].count >= targetX {
+        // print("Exploring to \(targetX), \(targetY), current size: \(cave.last?.count ?? 0),\(cave.count)")
+
+        if cave.count > targetY && cave[targetY].count > targetX {
             return
         }
 
-        if cave.count > 0 && cave[targetY].count < targetX {
+        // print("Exploring will expand: size of target row: \(cave.count > targetY ? cave[targetY].count : 0)")
+        if cave.count > 0 && cave.first!.count != cave.last!.count {
+            printCave()
+            fatalError("Fix please")
+        }        
+        if cave.count > 0 && cave[0].count <= targetX {
+            // print("Exploring to right")
             for y in (0..<cave.count) {
                 for x in (cave[y].count...targetX) {
                     cave[y].append(type(x: x, y: y))
                 } 
             }
         }
-        
-        for y in (cave.count...targetY) {
-            cave.append([])
-            for x in (0...targetX) {
-                cave[y].append(type(x: x, y: y))
+        if cave.count <= targetY {
+            // print("Exploring to below \(cave.count...targetY)")
+
+            for y in (cave.count...targetY) {
+                cave.append([])
+                // print(cave.first?.count ?? 0, targetX + 1)
+                for x in (0..<max(targetX + 1, cave.first?.count ?? 0)) {
+                    cave[y].append(type(x: x, y: y))
+                }
             }
         }
-
+        // print("Exploring, size afterwards:  \(cave.last?.count ?? 0),\(cave.count)" )
     }
 
     func type(x: Int, y: Int) -> CaveCell {
@@ -203,11 +228,20 @@ class Solver {
             let current = openSet.min {
                 let lhsScore = fScore[$0] ?? Int.max
                 let rhsScore = fScore[$1] ?? Int.max
-                return lhsScore < rhsScore || (lhsScore == rhsScore && ($0.y < $1.y || ($0.y == $1.y && $0.x < $1.x)))
+                return lhsScore < rhsScore //|| (lhsScore == rhsScore && ($0.y < $1.y || ($0.y == $1.y && $0.x < $1.x)))
             }!
             // let current = openSet.min { fScore[$0] ?? Int.max < fScore[$1] ?? Int.max }!
-            // print("current", current)
+            // print("current", current, fScore[current]!)
+            // if fScore[current]! > 45 {
+            //     print("openSet", openSet)
+            //     print("closedSet", closedSet)
+            //     print("cameFrom", cameFrom)
+            //     print("gScore", gScore)
+            //     print("fScore", fScore)
+            //     fatalError()
+            // }
             if current == goal {
+                print("total cost: ", gScore[current]!)
                 return reconstruct(cameFrom, current: current)
             }
 
@@ -216,21 +250,21 @@ class Solver {
 
             for neighbor in neighbors(current) {
                 // print("neighbor", neighbor)
-                if closedSet.contains(neighbor) {
+                if closedSet.contains(neighbor.loc) {
                     continue
                 }
 
-                let tentativeG = gScore[current]! // + 1000000 + neighbor.y * 100 + neighbor.x
+                let tentativeG = gScore[current]! + neighbor.cost// + 1000000 + neighbor.y * 100 + neighbor.x
 
-                if !openSet.contains(neighbor) {
-                    openSet.insert(neighbor)
-                } else if tentativeG >= gScore[neighbor] ?? Int.max {
+                if !openSet.contains(neighbor.loc) {
+                    openSet.insert(neighbor.loc)
+                } else if tentativeG >= gScore[neighbor.loc] ?? Int.max {
                     continue
                 }
                 // print("Adding neighbor")
-                cameFrom[neighbor] = current
-                gScore[neighbor] = tentativeG
-                fScore[neighbor] = tentativeG + heuristicEstimate(start: neighbor, goal: goal)
+                cameFrom[neighbor.loc] = current
+                gScore[neighbor.loc] = tentativeG
+                fScore[neighbor.loc] = tentativeG + heuristicEstimate(start: neighbor.loc, goal: goal)
                 // print("Added neighbor")
             }
         }
@@ -242,7 +276,7 @@ class Solver {
         return abs(start.x - goal.x) + abs(start.y - goal.y)
     }
 
-    func reconstruct(_ cameFrom: [Pt: Pt], current: Pt) -> [Pt] {
+    func reconstruct(_ cameFrom: [Location: Location], current: Location) -> [Location] {
         var current = current
         var totalPath = [current]
         while cameFrom[current] != nil {
@@ -252,33 +286,55 @@ class Solver {
         return totalPath
     }
 
-    func neighbors(_ pt: Pt) -> [Pt] {
-        var pts: [Pt] = []
-        if case .empty = field[pt.y - 1][pt.x] {
-            pts.append(Pt(x: pt.x, y: pt.y - 1))
-        }
-        if case .empty = field[pt.y][pt.x - 1] {
-            pts.append(Pt(x: pt.x - 1, y: pt.y))
-        }
-        if case .empty = field[pt.y][pt.x + 1] {
-            pts.append(Pt(x: pt.x + 1, y: pt.y))
-        }
-        if case .empty = field[pt.y + 1][pt.x] {
-            pts.append(Pt(x: pt.x, y: pt.y + 1))
-        }
-        // print("found neighbors", pts)
-        return pts
-    }
+    func neighbors(_ loc: Location) -> [(loc: Location, cost: Int)] {
+        var locs: [(loc: Location, cost: Int)] = []
+        let possibleNextTypes: [CType]
 
-    func hpSum() -> Int {
-        return field.reduce(0) { sumR, row in 
-            return sumR + row.reduce(0) { sumC, cell in 
-                if case .player(let p) = cell {
-                    return sumC + p.hp
-                }
-                return sumC
-            }
+        switch (loc.equipment, cave[loc.y][loc.x].type) {
+        case (.torch, .rocky), (.torch, .mouth), (.torch, .target):
+            locs.append((loc: Location(x:loc.x, y: loc.y, equipment: .climbing), cost:7))
+            possibleNextTypes = [.rocky, .narrow, .target]
+        case (.climbing, .rocky), (.climbing, .mouth), (.climbing, .target):
+            locs.append((loc: Location(x:loc.x, y: loc.y, equipment: .torch), cost:7))
+            possibleNextTypes = [.rocky, .wet, .target]
+        case (.climbing, .wet):
+            locs.append((loc: Location(x:loc.x, y: loc.y, equipment: .neither), cost:7))
+            possibleNextTypes = [.rocky, .wet, .target]
+        case (.neither, .wet):
+            locs.append((loc: Location(x:loc.x, y: loc.y, equipment: .climbing), cost:7))
+            possibleNextTypes = [.wet, .narrow]
+        case (.torch, .narrow):
+            locs.append((loc: Location(x:loc.x, y: loc.y, equipment: .neither), cost:7))
+            possibleNextTypes = [.rocky, .narrow, .target]
+        case (.neither, .narrow):
+            locs.append((loc: Location(x:loc.x, y: loc.y, equipment: .torch), cost:7))
+            possibleNextTypes = [.wet, .narrow]
+        default:
+            fatalError("Unexpected state: \(loc), \(cave[loc.y][loc.x])")
         }
+        // print("check up & left")
+        if loc.x > 0 && possibleNextTypes.contains(cave[loc.y][loc.x - 1].type) {
+            // print("check left")
+            locs.append((loc: Location(x: loc.x-1, y: loc.y, equipment: loc.equipment), cost: 1))
+        }
+        if loc.y > 0 && possibleNextTypes.contains(cave[loc.y - 1][loc.x].type) {
+            // print("check up")
+            locs.append((loc: Location(x: loc.x, y: loc.y - 1, equipment: loc.equipment), cost: 1))
+        }
+        
+        // print("explore next")
+        exploreTo(targetX: loc.x+1, targetY: loc.y+1)
+        // print("explore right and below")
+        if possibleNextTypes.contains(cave[loc.y][loc.x + 1].type) {
+            // print("explore right")
+            locs.append((loc: Location(x: loc.x+1, y: loc.y, equipment: loc.equipment), cost: 1))
+        }
+        if possibleNextTypes.contains(cave[loc.y+1][loc.x].type) {
+            // print("explore below")
+            locs.append((loc: Location(x: loc.x, y: loc.y+1, equipment: loc.equipment), cost: 1))
+        }
+        // print("found neighbors", locs)
+        return locs
     }
 }
 
@@ -292,3 +348,7 @@ if CommandLine.arguments.count > 1 {
 }
 
 print(result)
+
+// gScore [(5,3) 🧗: 22, (0,3) ∅: 17, (0,0) 🔥: 0, (2,0) 🔥: 16, (5,5) 🧗: 24, (1,0) ∅: 10, (7,1) ∅: 15, (3,4) 🧗: 28, (2,2) 🧗: 11, (5,3) 🔥: 22, (6,4) 🧗: 24, (3,2) ∅: 12, (5,1) 🔥: 20, (4,1) ∅: 12, (5,1) ∅: 13, (2,2) ∅: 11, (3,0) ∅: 12, (5,4) ∅: 30, (7,1) 🔥: 22, (4,1) 🧗: 19, (5,0) 🔥: 21, (2,0) 🧗: 9, (1,2) ∅: 10, (6,2) 🧗: 22, (1,3) 🧗: 11, (1,2) 🧗: 10, (3,1) ∅: 11, (4,0) ∅: 13, (3,2) 🔥: 19, (4,0) 🧗: 20, (5,2) 🔥: 21, (2,5) ∅: 28, (6,0) 🔥: 22, (0,2) 🧗: 9, (2,3) ∅: 12, (2,1) 🧗: 10, (3,4) 🔥: 21, (3,0) 🔥: 19, (0,1) 🧗: 8, (1,0) 🧗: 8, (1,3) 🔥: 18, (2,5) 🔥: 21, (1,4) 🔥: 19, (0,0) 🧗: 7, (0,5) ∅: 19, (3,1) 🔥: 18, (2,4) 🧗: 27, (2,1) ∅: 10, (6,1) 🔥: 21, (6,2) 🔥: 22, (0,1) 🔥: 1, (4,4) 🧗: 24, (5,2) 🧗: 21, (4,3) 🧗: 21, (6,1) ∅: 14, (1,4) ∅: 19, (6,3) 🔥: 30, (2,3) 🔥: 19, (0,5) 🧗: 12, (0,4) 🧗: 11, (2,4) 🔥: 20, (0,2) 🔥: 2, (0,4) ∅: 18, (1,1) 🔥: 2, (0,3) 🧗: 10, (5,4) 🧗: 23, (6,0) ∅: 15, (3,5) 🔥: 22, (3,3) 🔥: 20, (1,5) 🔥: 22, (4,2) 🧗: 20, (4,2) 🔥: 22, (1,1) ∅: 9, (6,3) 🧗: 23]
+// fScore [(5,3) 🧗: 34, (0,3) ∅: 34, (0,0) 🔥: 20, (2,0) 🔥: 34, (5,5) 🧗: 34, (1,0) ∅: 29, (7,1) ∅: 27, (3,4) 🧗: 41, (2,2) 🧗: 27, (5,3) 🔥: 34, (6,4) 🧗: 34, (3,2) ∅: 27, (5,1) 🔥: 34, (4,1) ∅: 27, (5,1) ∅: 27, (2,2) ∅: 27, (3,0) ∅: 29, (5,4) ∅: 41, (7,1) 🔥: 34, (4,1)🧗: 34, (5,0) 🔥: 36, (2,0) 🧗: 27, (1,2) ∅: 27, (6,2) 🧗: 34, (1,3) 🧗: 27, (1,2) 🧗: 27, (3,1) ∅: 27, (4,0) ∅: 29, (3,2) 🔥: 34, (4,0) 🧗: 36, (5,2) 🔥: 34, (2,5) ∅: 41, (6,0) 🔥: 36, (0,2) 🧗: 27, (2,3) ∅: 27, (2,1) 🧗: 27, (3,4) 🔥: 34, (3,0) 🔥: 36, (0,1) 🧗: 27, (1,0) 🧗: 27, (1,3) 🔥: 34, (2,5) 🔥: 34, (1,4) 🔥: 34, (0,0) 🧗: 27, (0,5) ∅: 34, (3,1) 🔥: 34, (2,4) 🧗: 41, (2,1) ∅: 27, (6,1) 🔥: 34, (6,2) 🔥: 34, (0,1) 🔥: 20, (4,4) 🧗: 36, (5,2) 🧗: 34, (4,3) 🧗: 34, (6,1) ∅: 27, (1,4) ∅: 34, (6,3) 🔥: 41, (2,3) 🔥: 34, (0,5) 🧗: 27, (0,4) 🧗: 27, (2,4) 🔥: 34, (0,2) 🔥: 20, (0,4) ∅: 34, (1,1) 🔥: 20, (0,3) 🧗: 27, (5,4) 🧗: 34, (6,0) ∅: 29, (3,5) 🔥: 34, (3,3) 🔥: 34, (1,5) 🔥: 36, (4,2) 🧗: 34, (4,2) 🔥: 36, (1,1) ∅: 27, (6,3) 🧗: 34]
+// current (4,3) 🧗
