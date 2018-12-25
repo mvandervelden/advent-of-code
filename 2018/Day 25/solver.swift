39bef64b1beae21f53a53f06215d8ecfe324918a
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2018-12-25 07:54:22 +0100

    Day 25

--- 2018/Day 25/solver.swift
+++ 2018/Day 25/solver.swift
@@ -13,16 +13,66 @@ class Solver {
         return "r1: \(result1)\nr2: \(result2)"
     }
 
+    typealias Point = [Int]
+
     private func solve1(input: String) -> String {
-        let lines = input.split(separator: "\n")
-        
-        return ""
+        let points: [Point] = input.split(separator: "\n").map { $0.split(separator: ",").map { Int($0)! } }
+        print(points)
+
+        var constellations: [[Point]] = []
+        for pt in points {
+            print(pt)
+            var matchingConstellations: Set<Int> = []
+            for constellation in constellations.enumerated() {
+                for cpt in constellation.element {
+                    if dist(p1: pt, p2: cpt) <= 3 {
+                        matchingConstellations.insert(constellation.offset)
+                    }
+                }
+            }
+
+            switch matchingConstellations.count {
+            case 0:
+                print("new const")
+                constellations.append([pt])
+            case 1:
+                print("appending to", matchingConstellations.first!)
+                constellations[matchingConstellations.first!].append(pt)
+            default:
+                print("mergin consts")
+                var newConstellation: [Point] = [pt]
+                print(matchingConstellations)
+                for i in matchingConstellations.sorted().reversed() {
+                    // print(i)
+                    print(constellations[i])
+                    newConstellation.append(contentsOf: constellations[i])
+                    constellations.remove(at: i)
+                }
+
+                print("merged const", newConstellation)
+                constellations.append(newConstellation)
+            }
+        }
+
+        constellations.forEach {
+            print($0)
+        }
+        return "\(constellations.count)"
     }
 
     private func solve2(input: String) -> String {
-        let lines = input.split(separator: "\n")
+        // let lines = input.split(separator: "\n")
 
         return ""
+    }
+
+    func dist(p1: Point, p2: Point) -> Int {
+        let d1: Int = abs(p1[0] - p2[0])
+        let d2: Int = abs(p1[1] - p2[1])
+        let d3: Int = abs(p1[2] - p2[2])
+        let d4: Int = abs(p1[3] - p2[3])
+
+        return d1 + d2 + d3 + d4
     }
 
     private func readFile(_ fileName: String) -> String {
