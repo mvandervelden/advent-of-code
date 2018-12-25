90e24f9f7b9d792faa8b69f208b73d144ef34e38
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2018-12-25 07:21:42 +0100

    Day 24 WIP

--- 2018/Day 24/solver.swift
+++ 2018/Day 24/solver.swift
@@ -4,7 +4,54 @@ import Foundation
 // $ swift solver.swift [inputfilename]
 // If no argument provided, it takes "input.txt"
 
+extension String {
+    /// An `NSRange` that represents the full range of the string.
+    var nsrange: NSRange {
+        return NSRange(location: 0, length: utf16.count)
+    }
+
+    /// Returns a substring with the given `NSRange`,
+    /// or `nil` if the range can't be converted.
+    func substring(with nsrange: NSRange) -> String? {
+        guard let range = Range(nsrange) else { return nil }
+        
+        let startIndex = self.index(self.startIndex, offsetBy: range.startIndex)
+        let stopIndex = self.index(self.startIndex, offsetBy: range.startIndex + range.count)
+        return String(self[startIndex..<stopIndex])
+    }
+
+    /// Returns a range equivalent to the given `NSRange`,
+    /// or `nil` if the range can't be converted.
+    func range(from nsrange: NSRange) -> Range<Index>? {
+        guard let range = Range(nsrange) else { return nil }
+        
+        let startIndex = self.index(self.startIndex, offsetBy: range.startIndex)
+        let stopIndex = self.index(self.startIndex, offsetBy: range.startIndex + range.count)
+
+        return startIndex..<stopIndex
+    }
+}
+
 class Solver {
+
+    enum Team {
+        case immune, infect
+    }
+
+    class Group: CustomStringConvertible {
+        let team: Team
+        var units: Int
+        let hp: Int
+        let weakness: [String]
+        let immune: [String]
+        let attack: String
+        let damage: Int
+        let initiative: Int
+
+        init(string: String) {
+
+        }
+    }
 
     func solve(_ fileName: String = "input.txt") -> String {
         let input = readFile(fileName)
@@ -15,12 +62,14 @@ class Solver {
 
     private func solve1(input: String) -> String {
         let lines = input.split(separator: "\n")
-        
+        if lines.count == 6 {
+
+        }
         return ""
     }
 
     private func solve2(input: String) -> String {
-        let lines = input.split(separator: "\n")
+        // let lines = input.split(separator: "\n")
 
         return ""
     }
