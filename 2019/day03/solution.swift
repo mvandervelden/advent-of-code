6ab9b66caeaa02711f59a1ca4d66fe01afe642e6
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2019-12-03 10:15:02 +0100

    Day03 part 1 WIP

--- 2019/day03/solution.swift
+++ 2019/day03/solution.swift
@@ -29,11 +29,156 @@ class Solution {
   }
 
   private func solveOne(file: File) -> String {
-    return "input: \(file.filename)\ncontent:\n\(file.string)\nresult 1"
+    let path1 = file.words.first!.vectors
+    let path2 = file.words.last!.vectors
+    var grid = Grid()
+
+    for vector in path1 {
+      grid.addVector(vector)
+    }
+
+    for vector in path2 {
+      grid.addVector(vector)
+    }
+    return "\(grid.description)"
   }
 
   private func solveTwo(file: File) -> String {
     return "input: \(file.filename)\ncontent:\n\(file.words)\nresult 2"
+  }
+}
+
+struct Grid {
+  var currentGrid: [Int: [Int: GridPoint]]
+  var currentCoordinates: (x: Int, y: Int) = (x: 0, y: 0)
+
+  init() {
+    currentGrid = [:]
+    currentGrid[0] = [:]
+    currentGrid[0]![0] = GridPoint.origin
+  }
+
+  mutating func addVector(_ vector: Vector) {
+    //swiftlint:disable identifier_name
+    switch vector.direction {
+    case .up:
+      for dy in 1..<vector.distance {
+        let oldPoint = currentGrid[currentCoordinates.x]![currentCoordinates.x - dy]
+        switch oldPoint {
+        case .none:
+          currentGrid[currentCoordinates.x]![currentCoordinates.x - dy] = .vPath
+        case .some(.hPath):
+          currentGrid[currentCoordinates.x]![currentCoordinates.x - dy] = .cross
+        default:
+          fatalError("""
+            unexpected value in grid.
+            coord: (\(currentCoordinates.x), \(currentCoordinates.x - dy)):
+            \(currentGrid[currentCoordinates.x]![currentCoordinates.x - dy]!),
+            for vector: \(vector)
+          """)
+        }
+      }
+      currentGrid[currentCoordinates.x]![currentCoordinates.y - vector.distance] = .corner
+      currentCoordinates = (x: currentCoordinates.x, y: currentCoordinates.y - vector.distance)
+    case .down:
+      for y in (currentCoordinates.y + 1)..<(currentCoordinates.y + vector.distance) {
+        let oldPoint = currentGrid[currentCoordinates.x]![y]
+        switch oldPoint {
+        case .none:
+          currentGrid[currentCoordinates.x]![y] = .vPath
+        case .some(.hPath):
+          currentGrid[currentCoordinates.x]![y] = .cross
+        default:
+          fatalError("unexpected value in grid. coord: (\(currentCoordinates.x), \(y)): \(currentGrid[currentCoordinates.x]![y]!), for vector: \(vector)")
+        }
+      }
+      currentGrid[currentCoordinates.x]![currentCoordinates.y + vector.distance] = .corner
+      currentCoordinates = (x: currentCoordinates.x, y: currentCoordinates.y + vector.distance)
+    case .left:
+      for dx in 1..<vector.distance {
+        if currentGrid[currentCoordinates.x - dx] == nil { currentGrid[currentCoordinates.x - dx] = [:] }
+
+        let oldPoint = currentGrid[currentCoordinates.x - dx]![currentCoordinates.y]
+        switch oldPoint {
+        case .none:
+          currentGrid[currentCoordinates.x - dx]![currentCoordinates.y] = .hPath
+        case .some(.vPath):
+          currentGrid[currentCoordinates.x - dx]![currentCoordinates.y] = .cross
+        default:
+          fatalError("""
+            unexpected value in grid.
+            coord: (\(currentCoordinates.x - dx), \(currentCoordinates.y)):
+            \(currentGrid[currentCoordinates.x - dx]![currentCoordinates.y]!),
+            for vector: \(vector)
+          """)
+        }
+      }
+      if currentGrid[currentCoordinates.x + vector.distance] == nil {
+        currentGrid[currentCoordinates.x + vector.distance] = [:]
+      }
+
+      currentGrid[currentCoordinates.x - vector.distance]![currentCoordinates.y] = .corner
+      currentCoordinates = (x: currentCoordinates.x - vector.distance, y: currentCoordinates.y)
+    case .right:
+      for x in (currentCoordinates.x + 1)..<(currentCoordinates.x + vector.distance) {
+        if currentGrid[x] == nil { currentGrid[x] = [:] }
+
+        let oldPoint = currentGrid[x]![currentCoordinates.y]
+        switch oldPoint {
+        case .none:
+          currentGrid[x]![currentCoordinates.y] = .hPath
+        case .some(.vPath):
+          currentGrid[x]![currentCoordinates.y] = .cross
+        default:
+          fatalError("unexpected value in grid. coord: (\(x), \(currentCoordinates.y)): \(currentGrid[x]![currentCoordinates.y]!), for vector: \(vector)")
+        }
+      }
+      if currentGrid[currentCoordinates.x + vector.distance] == nil {
+        currentGrid[currentCoordinates.x + vector.distance] = [:]
+      }
+
+      currentGrid[currentCoordinates.x + vector.distance]![currentCoordinates.y] = .corner
+      currentCoordinates = (x: currentCoordinates.x + vector.distance, y: currentCoordinates.y)
+    }
+    //swiftlint:enable identifier_name
+  }
+
+  var description: String {
+    return ""
+  }
+}
+
+enum GridPoint: Character {
+  case origin = "o"
+  case hPath = "-"
+  case vPath = "|"
+  case corner = "+"
+  case cross = "X"
+}
+
+enum Direction: Character {
+  //swiftlint:disable:next identifier_name
+  case up = "U"
+  case down = "D"
+  case left = "L"
+  case right = "R"
+}
+
+struct Vector {
+  let direction: Direction
+  let distance: Int
+
+  init(string: String) {
+    print(string)
+    direction = Direction(rawValue: string.first!)!
+    let distanceString = string.dropFirst()
+    distance = Int(distanceString)!
+  }
+}
+
+extension Array where Element == String {
+  var vectors: [Vector] {
+    return map(Vector.init)
   }
 }
 
@@ -57,7 +202,7 @@ class File {
   }()
 
   lazy var words: Words = {
-    return lines.map { $0.split(separator: " ").map(String.init) }
+    return lines.map { $0.split(separator: ",").map(String.init) }
   }()
 
   init(filename: String) {
