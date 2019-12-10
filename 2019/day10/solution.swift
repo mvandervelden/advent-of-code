021fc62ee9c177287044b5b840f8dc3cd308bff4
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2019-12-10 12:57:19 +0100

    Day 10 part 1

--- 2019/day10/solution.swift
+++ 2019/day10/solution.swift
@@ -29,7 +29,74 @@ class Solution {
   }
 
   private func solveOne(file: File) -> String {
-    return "input: \(file.filename)\ncontent:\n\(file.string)\nresult 1"
+    let input = file.lines.map(Array.init)
+    var grid: [Int: [Int: [(x: Int, y: Int, angle: Double)]]] = [:]
+
+    for (y, line) in input.enumerated() {
+      for (x, item) in line.enumerated() where item == "#" {
+        grid[y, default: [:]][x] = []
+      }
+    }
+
+    for (y, line) in grid {
+      for x in line.keys {
+        for (otherY, otherLine) in grid {
+          for otherX in otherLine.keys where !(otherX == x && otherY == y) {
+            let diff = (dx: otherX - x, dy: otherY - y)
+            let angle = atan2(Double(diff.dx), Double(diff.dy))
+            // if x == 2 && y == 2 {
+            //   print("testing (\(otherX),\(otherY))")
+            //   print("diff: \(diff)")
+            //   print("angle: \(angle)")
+            // }
+            var foundMatch = false
+            for (matchIndex, match) in grid[y]![x]!.enumerated() where angle == match.angle {
+              foundMatch = true
+              let matchDiff = (dx: match.x - x, dy: match.y - y)
+              if abs(matchDiff.dx) + abs(matchDiff.dy) > abs(diff.dx) + abs(diff.dy) {
+                grid[y]![x]![matchIndex] = (x: x, y: y, angle: angle)
+              }
+              break
+            }
+            if !foundMatch {
+              grid[y]![x]!.append((x: x, y: y, angle: angle))
+            }
+          }
+        }
+      }
+    }
+
+    var maxVal = 0
+    var max = (x: -1, y: -1)
+
+    for y in grid.keys {
+      for x in grid[y]!.keys {
+        let val = grid[y]![x]!.count
+        if val > maxVal {
+          maxVal = val
+          max = (x: x, y: y)
+        }
+      }
+    }
+
+    // let minY = grid.keys.min()!
+    // let maxY = grid.keys.max()!
+    // let minX = grid.values.flatMap { $0.keys }.min()!
+    // let maxX = grid.values.flatMap { $0.keys }.max()!
+
+    // for y in minY...maxY {
+    //   for x in minX...maxX {
+    //     if let val = grid[y]?[x]?.count {
+    //       print("\(val) ", terminator: "")
+    //     } else {
+    //       print(" .  ", terminator: "")
+    //     }
+    //   }
+    //   print("")
+    // }
+
+    return "max: \(maxVal), coord: (\(max.x),\(max.y))"
+    // return "input: \(file.filename)\ncontent:\n\(file.string)\nresult 1"
   }
 
   private func solveTwo(file: File) -> String {
