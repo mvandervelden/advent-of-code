456838f68940d6df06726bd38409e0416374a644
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2019-12-10 17:42:23 +0100

    Finished Day 10 part 2

--- 2019/day10/solution.swift
+++ 2019/day10/solution.swift
@@ -96,11 +96,90 @@ class Solution {
     // }
 
     return "max: \(maxVal), coord: (\(max.x),\(max.y))"
-    // return "input: \(file.filename)\ncontent:\n\(file.string)\nresult 1"
   }
 
   private func solveTwo(file: File) -> String {
-    return "input: \(file.filename)\ncontent:\n\(file.words)\nresult 2"
+    let input = file.lines.map(Array.init)
+    var grid: [Int: [Int: [(x: Int, y: Int, angle: Double)]]] = [:]
+
+    for (y, line) in input.enumerated() {
+      for (x, item) in line.enumerated() where item == "#" {
+        grid[y, default: [:]][x] = []
+      }
+    }
+
+    let x = 11
+    let y = 13
+    var angles: [(angle: Double, asteroids: [(dist: Int, x: Int, y: Int)])] = []
+
+    for (otherY, otherLine) in grid {
+      for otherX in otherLine.keys where !(otherX == x && otherY == y) {
+        let diff = (dx: otherX - x, dy: otherY - y)
+        let angle = atan2(Double(diff.dx), Double(diff.dy))
+        let dist = abs(diff.dx) + abs(diff.dy)
+
+        if let existingAngle = angles.enumerated().first(where: { $0.element.angle == angle }) {
+          angles[existingAngle.offset].asteroids.append((dist: dist, x: otherX, y: otherY))
+        } else {
+          angles.append((angle: angle, asteroids: [(dist: dist, x: otherX, y: otherY)]))
+        }
+      }
+    }
+
+    // .#.
+    // ###
+    // .#.
+    // angles: up: π
+    //         right: π/2
+    //         down: 0
+    //         left: -π/2
+    //         up: -π
+
+    for i in 0..<angles.count {
+      angles[i].asteroids = angles[i].asteroids.sorted { $0.dist < $1.dist }
+    }
+
+    angles.sort { $0.angle > $1.angle }
+
+    var destroyed = 0
+    var currentIndex = 0
+    var lastDestroyed = (dist: -1, x: -1, y: -1)
+
+    while destroyed < 200 && angles.count > 0 {
+      var current = angles[currentIndex]
+      lastDestroyed = current.asteroids.first!
+      print("destroyed: angle: \(current.angle): \(lastDestroyed)")
+      current.asteroids.removeFirst()
+      destroyed += 1
+
+      if current.asteroids.isEmpty {
+        angles.remove(at: currentIndex)
+      } else {
+        angles[currentIndex] = current
+        currentIndex += 1
+      }
+      if angles.count == 0 { break }
+      currentIndex = currentIndex % angles.count
+    }
+
+
+    // let minY = grid.keys.min()!
+    // let maxY = grid.keys.max()!
+    // let minX = grid.values.flatMap { $0.keys }.min()!
+    // let maxX = grid.values.flatMap { $0.keys }.max()!
+
+    // for y in minY...maxY {
+    //   for x in minX...maxX {
+    //     if let val = grid[y]?[x]?.count {
+    //       print("\(val) ", terminator: "")
+    //     } else {
+    //       print(" .  ", terminator: "")
+    //     }
+    //   }
+    //   print("")
+    // }
+
+    return "\(lastDestroyed)"
   }
 }
 
