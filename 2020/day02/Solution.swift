ec7874b5491045c9137466b8527ed623d89a5bd5
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2020-12-02 09:16:09 +0100

    Day 2

--- 2020/day02/Solution.swift
+++ 2020/day02/Solution.swift
@@ -3,20 +3,67 @@
 // part: either `1` or `2`
 // If no inputfilename provided, it takes "input.txt"
 
+struct Rule: CustomStringConvertible {
+  let letter: Character
+  let lowerRange: Int
+  let upperRange: Int
+
+  var description: String { "\(lowerRange)-\(upperRange) \(letter)" }
+
+  init(_ string: String) {
+    let parts = string.split(separator: " ")
+    letter = parts.last!.first!
+    let rangeParts = parts.first!.split(separator: "-")
+    lowerRange = Int(rangeParts.first!)!
+    upperRange = Int(rangeParts.last!)!
+  }
+}
+
+
+
+struct Entry: CustomStringConvertible {
+  let rule: Rule
+  let pwd: String
+
+  var description: String { "\(rule): \(pwd)" }
+
+  init(_ string: String) {
+    let parts = string.split(separator: ":")
+    rule = Rule(String(parts.first!))
+    pwd = String(parts.last!.dropFirst())
+  }
+
+  var isValid1: Bool {
+    let amountFound = pwd.reduce(0) { $0 + ($1 == rule.letter ? 1 : 0) }
+    return amountFound >= rule.lowerRange && amountFound <= rule.upperRange
+  }
+
+  var isValid2: Bool {
+    let lowerChar = pwd[pwd.index(pwd.startIndex, offsetBy: rule.lowerRange - 1)]
+    let upperChar = pwd[pwd.index(pwd.startIndex, offsetBy: rule.upperRange - 1)]
+    return [lowerChar, upperChar].filter { $0 == rule.letter }.count == 1
+  }
+}
+
 class Solution: Solving {
   let file: File
 
+  let entries: [Entry]
+
   required init(file: File) {
     self.file = file
+    entries = file.lines.map(Entry.init)
   }
 
   func solve1() -> String {
+    let validCount = entries.reduce(0) { $0 + ($1.isValid1 ? 1 : 0) }
 
-    return file.lines.joined(separator: "\n")
+    return validCount.description
   }
 
   func solve2() -> String {
+    let validCount = entries.reduce(0) { $0 + ($1.isValid2 ? 1 : 0) }
 
-    return file.lines.joined(separator: "\n")
+    return validCount.description
   }
 }
