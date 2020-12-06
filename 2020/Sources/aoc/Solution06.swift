3b472d18aa71f1fc70c80efcfb4a1e4c93cc8279
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2020-12-06 08:35:46 +0100

    Day 6, imprroved

--- 2020/Sources/aoc/Solution06.swift
+++ 2020/Sources/aoc/Solution06.swift
@@ -6,32 +6,31 @@ class Solution06: Solving {
   }
 
   func solve1() -> String {
-    let groups = file.wordsSplitByEmptyLine
-
-    var counts: [Int] = []
-    for group in groups {
-      let charArray: [Character] = group.flatMap(Array.init)
-      counts.append(Set(charArray).count)
-    }
-    return counts.sum().description
+    return file
+      .wordsSplitByEmptyLine
+      .lazy
+      .map { $0.flatMap(Array.init) }
+      .map(Set.init)
+      .map(\.count)
+      .sum()
+      .description
   }
 
   func solve2() -> String {
     let groups = file.wordsSplitByEmptyLine
 
-    var counts: [Int] = []
-    for group in groups {
-      let groupSize = group.count
-      let charArray: [Character] = group.flatMap(Array.init)
-      let charSet = Set(charArray)
+    return groups
+      .map { group in
+        let groupSize = group.count
+        let charArray = group.flatMap(Array.init)
+        let charSet = Set(charArray)
 
-      var valids = 0
-      for char in charSet {
-        let occurrences = charArray.count { $0 == char }
-        valids += (occurrences >= groupSize) ? 1 : 0
+        return charSet
+          .map { char in charArray.count { $0 == char } }
+          .map { $0 >= groupSize }
+          .count { $0 }
       }
-      counts.append(valids)
-    }
-    return counts.sum().description
+      .sum()
+      .description
   }
 }
