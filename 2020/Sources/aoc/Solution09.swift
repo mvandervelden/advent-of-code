aeb8febc29e3d601beb1f8b1a5ab46026fa58198
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2020-12-09 06:31:32 +0100

    Day 9

--- 2020/Sources/aoc/Solution09.swift
+++ 2020/Sources/aoc/Solution09.swift
@@ -1,17 +1,61 @@
 class Solution09: Solving {
   let file: File
+  let preambleSize: Int
 
   required init(file: File) {
     self.file = file
+    preambleSize = file.filename.contains("example") ? 5 : 25
   }
 
   func solve1() -> String {
-
-    return file.lines.joined(separator: "\n")
+    let vals = file.lines.map { Int($0)! }
+    let result = findInvalidNumber(vals: vals)
+    return result.description
   }
 
   func solve2() -> String {
+    let vals = file.lines.map { Int($0)! }
+    let invalidVal = findInvalidNumber(vals: vals)
+    var result = -1
+    for lowerBound in 0..<vals.count {
+      for upperBound in (lowerBound + 1)..<vals.count {
+        let range = vals[lowerBound...upperBound]
 
-    return file.lines.joined(separator: "\n")
+        if range.sum() == invalidVal {
+          let lowerVal = vals[lowerBound...upperBound].min()!
+          let upperVal = vals[lowerBound...upperBound].max()!
+          result = lowerVal + upperVal
+          break
+        } else if range.sum() > invalidVal {
+          break
+        }
+      }
+
+      if result > -1 { break }
+    }
+    return result.description
+  }
+
+  func findInvalidNumber(vals: [Int]) -> Int {
+    var result = -1
+    for (index, val) in vals[preambleSize...].enumerated() {
+      let i = index + preambleSize
+
+      var foundSum = false
+      let prev = vals[(i - preambleSize)..<i]
+      for p1 in prev {
+        for p2 in prev {
+          if p1 != p2, p1 + p2 == val {
+            foundSum = true
+          }
+        }
+      }
+
+      if !foundSum {
+        result = val
+        break
+      }
+    }
+    return result
   }
 }
