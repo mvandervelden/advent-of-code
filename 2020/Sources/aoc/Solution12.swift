295edf8588306d5984e50966b3abd15755d146b3
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2020-12-12 06:59:47 +0100

    Day 12 Some cleanup

--- 2020/Sources/aoc/Solution12.swift
+++ 2020/Sources/aoc/Solution12.swift
@@ -16,38 +16,22 @@ class Solution12: Solving {
 
     for (inst, val) in zip(insts, vals) {
       switch (inst, dir, val) {
-      case ("F", "E", _): x += val
-      case ("F", "W", _): x -= val
-      case ("F", "N", _): y -= val
-      case ("F", "S", _): y += val
-      case ("E", _, _): x += val
-      case ("W", _, _): x -= val
-      case ("N", _, _): y -= val
-      case ("S", _, _): y += val
-      case ("L", "E", 90): dir = "N"
-      case ("L", "E", 180): dir = "W"
-      case ("L", "E", 270): dir = "S"
-      case ("R", "E", 90): dir = "S"
-      case ("R", "E", 180): dir = "W"
-      case ("R", "E", 270): dir = "N"
-      case ("L", "W", 90): dir = "S"
-      case ("L", "W", 180): dir = "E"
-      case ("L", "W", 270): dir = "N"
-      case ("R", "W", 90): dir = "N"
-      case ("R", "W", 180): dir = "E"
-      case ("R", "W", 270): dir = "S"
-      case ("L", "N", 90): dir = "W"
-      case ("L", "N", 180): dir = "S"
-      case ("L", "N", 270): dir = "E"
-      case ("R", "N", 90): dir = "E"
-      case ("R", "N", 180): dir = "S"
-      case ("R", "N", 270): dir = "W"
-      case ("L", "S", 90): dir = "E"
-      case ("L", "S", 180): dir = "N"
-      case ("L", "S", 270): dir = "W"
-      case ("R", "S", 90): dir = "W"
-      case ("R", "S", 180): dir = "N"
-      case ("R", "S", 270): dir = "E"
+      case ("F", "E", _), ("E", _, _): x += val
+      case ("F", "W", _), ("W", _, _): x -= val
+      case ("F", "N", _), ("N", _, _): y -= val
+      case ("F", "S", _), ("S", _, _): y += val
+      case ("L", "E",  90), ("R", "E", 270),
+           ("L", "W", 270), ("R", "W",  90),
+           ("L", "S", 180), ("R", "S", 180): dir = "N"
+      case ("L", "E", 180), ("R", "E", 180),
+           ("L", "N",  90), ("R", "N", 270),
+           ("L", "S", 270), ("R", "S",  90): dir = "W"
+      case ("L", "E", 270), ("R", "E",  90),
+           ("L", "W",  90), ("R", "W", 270),
+           ("L", "N", 180), ("R", "N", 180): dir = "S"
+      case ("L", "W", 180), ("R", "W", 180),
+           ("L", "N", 270), ("R", "N",  90),
+           ("L", "S",  90), ("R", "S", 270): dir = "E"
       default: preconditionFailure("Unexpected input: \(inst)\(val)")
       }
     }
@@ -67,16 +51,16 @@ class Solution12: Solving {
 
     for (inst, val) in zip(insts, vals) {
       switch (inst, val, xWP, yWP) {
-      case ("F", _, _, _):
-        xShip += xWP * val
-        yShip += yWP * val
+      case ("F", _, _, _): xShip += xWP * val; yShip += yWP * val
       case ("E", _, _, _): xWP += val
       case ("W", _, _, _): xWP -= val
       case ("N", _, _, _): yWP -= val
       case ("S", _, _, _): yWP += val
-      case ("L", 90, let px, let py), ("R", 270, let px, let py): xWP = py; yWP = -px
-      case ("L", 270, let px, let py), ("R", 90, let px, let py): xWP = -py; yWP = px
-      case (_, 180, let px, let py): xWP = -px; yWP = -py
+      case ("L",  90, let px, let py),
+           ("R", 270, let px, let py): xWP = py; yWP = -px
+      case ("L", 270, let px, let py),
+           ("R",  90, let px, let py): xWP = -py; yWP = px
+      case (  _, 180, let px, let py): xWP = -px; yWP = -py
       default: preconditionFailure("Unexpected input: \(inst)\(val)")
       }
     }
