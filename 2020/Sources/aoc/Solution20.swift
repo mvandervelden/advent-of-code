444944840045652604e9bc186d3dad76a17500ee
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2020-12-22 08:18:17 +0100

    Day 20 part 1 cleanup 2

--- 2020/Sources/aoc/Solution20.swift
+++ 2020/Sources/aoc/Solution20.swift
@@ -1,12 +1,3 @@
-extension Collection where Self.Iterator.Element: RandomAccessCollection {
-  func transposed() -> [[Self.Iterator.Element.Iterator.Element]] {
-    guard let firstRow = self.first else { return [] }
-    return firstRow.indices.map { index in
-      self.map{ $0[index] }
-    }
-  }
-}
-
 class Solution20: Solving {
   typealias T = [[Character]]
 
