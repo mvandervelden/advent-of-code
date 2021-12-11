c2f6eb56877d5098c508f1925e9efbd99877e6f3
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2021-12-11 10:45:30 +0100

    Day 11, removed unnecessary loop detection

--- 2021/Sources/aoc/Solution11.swift
+++ 2021/Sources/aoc/Solution11.swift
@@ -8,22 +8,8 @@ class Solution11: Solving {
   func solve1() -> String {
     var octos = file.charsByLine.map { line in line.map { Int(String($0))! } }
     var score = 0
-    var history: [[[Int]]: (i: Int, score: Int)] = [:]
 
-    for i in 1...100 {
-      if let oldScore = history[octos] {
-        let addition = score - oldScore.score
-        let steps = i - oldScore.i
-        let cycles = (100 - i) / steps
-        score += cycles * addition
-        let leftover = 100 - i + cycles * steps
-        let leftoverScore = history.values.first { $0.i == oldScore.i + leftover }!.score - oldScore.score
-        score += leftoverScore
-        break
-      } else {
-        history[octos] = (i: i - 1, score: score)
-      }
-
+    for _ in 1...100 {
       var hits: [Point2D] = []
 
       for y in 0..<octos.count {
@@ -60,22 +46,8 @@ class Solution11: Solving {
   func solve2() -> String {
 var octos = file.charsByLine.map { line in line.map { Int(String($0))! } }
     var score = 0
-    var history: [[[Int]]: (i: Int, score: Int)] = [:]
 
-    for i in 1...1000 {
-      if let oldScore = history[octos] {
-        let addition = score - oldScore.score
-        let steps = i - oldScore.i
-        let cycles = (100 - i) / steps
-        score += cycles * addition
-        let leftover = 100 - i + cycles * steps
-        let leftoverScore = history.values.first { $0.i == oldScore.i + leftover }!.score - oldScore.score
-        score += leftoverScore
-        break
-      } else {
-        history[octos] = (i: i - 1, score: score)
-      }
-
+    for i in 1...250 {
       var hits: [Point2D] = []
       var hitCount = 0
       for y in 0..<octos.count {
