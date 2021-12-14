50bc46de8ad71e7b3dacc637bf3b15330e33341b
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2021-12-14 10:02:01 +0100

    Day 14 part 1, part 2 too slow

--- 2021/Sources/aoc/Solution14.swift
+++ 2021/Sources/aoc/Solution14.swift
@@ -6,10 +6,92 @@ class Solution14: Solving {
   }
 
   func solve1() -> String {
-    return "TBD"
+    let input = file.lines
+
+    var polymer = Array(input[0])
+
+    let rules = input[2...].reduce(into: [String: Character]()) { dict, line in
+      let parts = line.components(separatedBy: " -> ")
+      dict[String(parts[0])] = parts[1].first!
+    }
+
+    print(String(polymer))
+
+    for _ in 0..<10 {
+      var i = 0
+      while i < polymer.count - 1 {
+        let part = String(polymer[i...i+1])
+        if let substitute = rules[part] ?? rules[String(part.reversed())] {
+          polymer.insert(substitute, at: i+1)
+          i+=2
+        } else {
+          print("no rule for: \(part)")
+          i+=1
+        }
+      }
+      // print(String(polymer))
+    }
+
+    let counts = polymer.reduce(into: [Character: Int]()) { $0[$1, default: 0] += 1 }
+
+    var max: Character = " "
+    var maxCount = 0
+    var min: Character = " "
+    var minCount = Int.max
+
+    for (char, count) in counts {
+      if count > maxCount { maxCount = count; max = char }
+      if count < minCount { minCount = count; min = char }
+    }
+    print(String(polymer))
+    print(counts)
+
+
+    return (maxCount - minCount).description
   }
 
   func solve2() -> String {
-    return "TBD"
-  }
+    let input = file.lines
+
+    var polymer = Array(input[0])
+
+    let rules = input[2...].reduce(into: [String: Character]()) { dict, line in
+      let parts = line.components(separatedBy: " -> ")
+      dict[String(parts[0])] = parts[1].first!
+    }
+
+    print(String(polymer))
+
+    for n in 0..<40 {
+      var i = 0
+      while i < polymer.count - 1 {
+        let part = String(polymer[i...i+1])
+        if let substitute = rules[part] ?? rules[String(part.reversed())] {
+          polymer.insert(substitute, at: i+1)
+          i+=2
+        } else {
+          print("no rule for: \(part)")
+          i+=1
+        }
+      }
+      print(n)
+      // print(String(polymer))
+    }
+
+    let counts = polymer.reduce(into: [Character: Int]()) { $0[$1, default: 0] += 1 }
+
+    var max: Character = " "
+    var maxCount = 0
+    var min: Character = " "
+    var minCount = Int.max
+
+    for (char, count) in counts {
+      if count > maxCount { maxCount = count; max = char }
+      if count < minCount { minCount = count; min = char }
+    }
+    print(String(polymer))
+    print(counts)
+
+
+    return (maxCount - minCount).description  }
 }
