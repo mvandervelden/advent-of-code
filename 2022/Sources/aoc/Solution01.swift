59148d107395cc583f62e20c72c5162f02565319
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2022-12-01 07:34:53 +0100

    2022 day 1, simplified

--- 2022/Sources/aoc/Solution01.swift
+++ 2022/Sources/aoc/Solution01.swift
@@ -12,24 +12,7 @@ class Solution01: Solving {
   }
 
   func solve2() -> String {
-    let calories = file.intsSplitByEmptyLine
-
-    var max3 = [0, 0, 0]
-
-    for cal in calories {
-      let sum = cal.sum()
-      if sum <= max3[2] { continue }
-      else if sum > max3[0] {
-        max3.insert(sum, at: 0)
-      } else if sum > max3[1] {
-        max3.insert(sum, at: 1)
-      } else if sum > max3[2] {
-        max3.insert(sum, at: 2)
-      }
-      max3.removeLast()
-    }
-
-    return max3.sum().description
+    return file.intsSplitByEmptyLineSummed.sorted().suffix(3).sum().description
   }
 }
 
@@ -37,6 +20,12 @@ extension File {
   var intsSplitByEmptyLine: [[Int]] {
     return string.components(separatedBy: "\n\n").map { (block: String) in
       block.split(separator: "\n").compactMap { Int($0) }
+    }
+  }
+
+  var intsSplitByEmptyLineSummed: [Int] {
+    return string.components(separatedBy: "\n\n").map { (block: String) in
+      block.split(separator: "\n").compactMap { Int($0) }.sum()
     }
   }
 }
