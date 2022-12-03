9cdce7f5c454d97a98d510a17f88a6e9ecc06921
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2022-12-03 09:24:48 +0100

    2022 day 3

--- 2022/Sources/aoc/Solution03.swift
+++ 2022/Sources/aoc/Solution03.swift
@@ -6,11 +6,43 @@ class Solution03: Solving {
   }
 
   func solve1() -> String {
-    return file.filename
+    return file.lines.reduce(0) {
+      let chars = Array($1)
+      let first = Set(chars[..<(chars.count / 2)])
+      let last = Set(chars[(chars.count / 2)...])
+      let char = first.intersection(last).first!
+      return $0 + Int(char.priority)
+    }.description
   }
 
   func solve2() -> String {
-    return file.filename
+    let sacks = file.charsByLine
+    let groups = sacks.enumerated().reduce([[[Character]]]()) {
+      switch $1.offset % 3 {
+      case 0:
+        return $0 + [[$1.element]]
+      default:
+        return $0[..<($0.count - 1)] + [$0.last! + [$1.element]]
+      }
+    }
+
+    return groups.reduce(0) {
+      let intersection = Set($1[0]).intersection(Set($1[1])).intersection(Set($1[2]))
+      if intersection.count > 1  { print($1.map { String($0.sorted()) }.joined(separator: "\n")); print(intersection) }
+      return $0 + Int(intersection.first!.priority)
+    }.description
   }
 }
 
+extension Character {
+  var priority: Int {
+    switch asciiValue! {
+      case 65...90:
+        return Int(asciiValue!) - 65 + 27
+      case 97...122:
+        return Int(asciiValue!) - 97 + 1
+      default:
+        fatalError("unexpected input")
+    }
+  }
+}
