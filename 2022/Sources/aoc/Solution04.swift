3098bff35ba71c02ae0fcf9da8876b6bf203a2cc
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2022-12-04 07:43:03 +0100

    2022 day 4

--- 2022/Sources/aoc/Solution04.swift
+++ 2022/Sources/aoc/Solution04.swift
@@ -6,11 +6,37 @@ class Solution04: Solving {
   }
 
   func solve1() -> String {
-    return file.filename
+    return file.csv.reduce(0) { sum, line in
+      let pair: [ClosedRange<Int>] = line.map { assignmentStr in
+        let ints = assignmentStr.split(separator: "-").map { Int($0)! }
+        return ints[0]...ints[1]
+      }
+      .sorted { $0.count > $1.count } // largest range always comes first
+
+      if pair[0].contains(pair[1].lowerBound) && pair[0].contains(pair[1].upperBound) {
+        return sum + 1
+      }
+
+      return sum
+    }
+    .description
   }
 
   func solve2() -> String {
-    return file.filename
+    return file.csv.reduce(0) { sum, line in
+      let pair: [ClosedRange<Int>] = line.map { assignmentStr in
+        let ints = assignmentStr.split(separator: "-").map { Int($0)! }
+        return ints[0]...ints[1]
+      }
+      .sorted { $0.count > $1.count } // largest range always comes first
+
+      if pair[0].overlaps(pair[1]) {
+        return sum + 1
+      }
+
+      return sum
+    }
+    .description
   }
 }
 
