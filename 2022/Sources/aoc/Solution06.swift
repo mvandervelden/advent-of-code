868cd90ff6caf5071d130a252572c35c2fc31d43
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2022-12-06 07:35:32 +0100

    2022 day 6

--- 2022/Sources/aoc/Solution06.swift
+++ 2022/Sources/aoc/Solution06.swift
@@ -6,11 +6,37 @@ class Solution06: Solving {
   }
 
   func solve1() -> String {
-    return file.filename
+ var result = 0
+
+    for line in file.charsByLine {
+      result = findStartMarker(input: line, windowSize: 4)
+    }
+
+    return result.description
   }
 
   func solve2() -> String {
-    return file.filename
+    var result = 0
+
+    for line in file.charsByLine {
+      result = findStartMarker(input: line, windowSize: 14)
+    }
+
+    return result.description
+  }
+
+  private func findStartMarker(input: [Character], windowSize: Int) -> Int {
+    let offset = windowSize - 1
+
+    for i in offset..<input.count {
+      let section = input[(i-offset)...i]
+      if Set(section).count == section.count {
+        print(String(section), i + 1)
+        return i + 1
+      }
+    }
+
+    return -1
   }
 }
 
