29df6c24a4a99b46aa6f445a6101dd8c2d3ef71d
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2022-12-07 09:13:18 +0100

    2022 day 7, cleanup

--- 2022/Sources/aoc/Solution07.swift
+++ 2022/Sources/aoc/Solution07.swift
@@ -1,90 +1,29 @@
-enum Output: CustomStringConvertible {
-  case dir(name: String)
-  case file(name: String, size: Int)
-
-  var isFile: Bool {
-    switch self {
-    case .dir: return false
-    case .file: return true
-    }
-  }
-
-  var size: Int {
-    switch self {
-      case .dir: return 0
-      case .file(_, let size): return size
-    }
-  }
-
-  // var node: Node {
-  //   switch self {
-  //   case .file(let name, let size):
-  //     return .file(name: name, size: size)
-  //   case .folder(let name):
-  //     return .folder([], name: name)
-  //   }
-  // }
+struct LSFile: CustomStringConvertible {
+  let name: String
+  let size: Int
 
   init(_ elems: [String]) {
-    if elems[0] == "dir" {
-      self = .dir(name: elems[1])
-    } else {
-      self = .file(name: elems[1], size: Int(elems[0])!)
-    }
+    name = elems[1]
+    size = Int(elems[0])!
   }
 
   var description: String {
-    switch self {
-    case .dir(let name): return "dir \(name)"
-    case .file(let name, let size): return "\(size) \(name)"
-    }
+    "\(size) \(name)"
   }
 }
 
-enum Command: CustomStringConvertible {
-  case cd(folder: String)
-  case ls(path: [String], output: [Output])
+struct Folder: CustomStringConvertible {
+  let path: [String]
+  let files: [LSFile]
 
   var description: String {
-    switch self {
-    case .cd(let folder): return "$ cd \(folder)"
-    case .ls(_, let output): return "$ ls\n\(output.map(\.description).joined(separator: "\n"))"
-    }
+    "\(path.joined(separator: "/"))\n\(files.map(\.description).joined(separator: "\n"))"
   }
 
   var fileSize: Int {
-    if case .ls(_, let output) = self {
-      return output.map(\.size).sum()
-    }
-    fatalError()
-  }
-
-  var hasFolders: Bool {
-    if case .ls(_, let output) = self {
-      return !output.allSatisfy(\.isFile)
-    }
-    fatalError()
+    files.map(\.size).sum()
   }
 }
-
-// enum Node: CustomStringConvertible {
-//   case folder([Node], name: String)
-//   case file(name: String, size: Int)
-
-//   var size: Int {
-//     case file(_, let size): return size
-//     case folder(let nodes): return nodes.map(\.size).sum()
-//   }
-
-//   var description: String {
-//     switch self {
-//     case .file(let name, let size):
-//       return "- \(name) (file, size=\(size))"
-//     case .folder(let nodes, let name):
-//       return "- \(name) (dir)\n" + nodes.map(\.description).map { "  " + $0 }.joined(separator: "\n")
-//     }
-//   }
-// }
 
 class Solution07: Solving {
   let file: File
@@ -95,8 +34,8 @@ class Solution07: Solving {
 
   func solve1() -> String {
     let output = file.words
-    let commands = parseOutput(output)
-    let sizes = getSizes(commands)
+    let folders = parseOutput(output)
+    let sizes = getSizes(folders)
 
     let max = 100000
 
@@ -107,8 +46,8 @@ class Solution07: Solving {
 
   func solve2() -> String {
     let output = file.words
-    let commands = parseOutput(output)
-    let sizes = getSizes(commands)
+    let folders = parseOutput(output)
+    let sizes = getSizes(folders)
 
     let totalSpace = 70000000
     let minFree = 30000000
@@ -120,21 +59,20 @@ class Solution07: Solving {
     return result!.description
   }
 
-  private func parseOutput(_ output: [[String]]) -> [Command] {
-    var commands: [Command] = []
-    var thisLS: [Output]? = nil
+  private func parseOutput(_ output: [[String]]) -> [Folder] {
+    var folders: [Folder] = []
+    var thisLS: [LSFile]? = nil
 
     var currentFolder: [String] = []
 
     for line in output {
       if line[0] == "$" {
         if let ls = thisLS {
-          commands.append(.ls(path: currentFolder, output: ls))
+          folders.append(Folder(path: currentFolder, files: ls))
           thisLS = nil
         }
 
         if line[1] == "cd" {
-          commands.append(.cd(folder: line[2]))
           if line[2] == ".." {
             _ = currentFolder.popLast()
           } else {
@@ -143,27 +81,25 @@ class Solution07: Solving {
         } else { // ls
           thisLS = []
         }
-      } else {
-        thisLS?.append(Output(line))
+      } else if line[0] != "dir" {
+        thisLS?.append(LSFile(line))
       }
     }
 
     if let ls = thisLS {
-      commands.append(.ls(path: currentFolder, output: ls))
+      folders.append(Folder(path: currentFolder, files: ls))
     }
 
-    return commands
+    return folders
   }
 
-  private func getSizes(_ commands: [Command]) -> [String: Int] {
+  private func getSizes(_ folders: [Folder]) -> [String: Int] {
     var sizes: [String: Int] = [:]
 
-    for command in commands {
-      if case .ls(let path, _) = command {
-        for i in 0..<path.count {
-          let subPath = path[0...i]
-          sizes[subPath.joined(separator:"/"), default: 0] += command.fileSize
-        }
+    for folder in folders {
+      for i in 0..<folder.path.count {
+        let subPath = folder.path[0...i]
+        sizes[subPath.joined(separator:"/"), default: 0] += folder.fileSize
       }
     }
 
