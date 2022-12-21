d32ed58be8a550d21ef626dceace5407eba21190
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2022-12-21 10:43:30 +0100

    2022 day 20 part 1 broken on input

--- 2022/Sources/aoc/Solution20.swift
+++ 2022/Sources/aoc/Solution20.swift
@@ -6,7 +6,43 @@ class Solution20: Solving {
   }
 
   func solve1() -> String {
-    return file.filename
+    var list = file.lines.map { Int($0)! }
+    var indices = Array(0..<list.count)
+
+    for i in 0..<list.count {
+      // print("i", i)
+      // print(list)
+      // print(indices)
+      let val = list[indices[i]]
+      // print("val", val)
+      var newIndex = indices[i] + val
+      // print("newIndex", newIndex)
+      list.remove(at: indices[i])
+
+      ((i+1)..<indices.count).forEach {
+        indices[$0] -= 1
+      }
+
+      let mod = newIndex % list.count
+      newIndex = mod >= 0 ? mod : mod + list.count
+
+      list.insert(val, at: newIndex)
+      ((newIndex+1)..<indices.count).forEach {
+        indices[$0] += 1
+      }
+    }
+
+    // print(list)
+    // print(indices)
+
+    let index0 = list.firstIndex(of: 0)!
+    // print(index0)
+    let s = [1000, 2000, 3000].map {
+      print(list[(index0 + $0) % list.count])
+      return list[(index0 + $0) % list.count]
+    }.sum()
+
+    return s.description
   }
 
   func solve2() -> String {
