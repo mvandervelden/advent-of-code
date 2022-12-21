ddd1f7607567f9c5491d4ebe4b0167c9b5275fc8
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2022-12-21 11:13:57 +0100

    2022 day 20 done

--- 2022/Sources/aoc/Solution20.swift
+++ 2022/Sources/aoc/Solution20.swift
@@ -5,48 +5,84 @@ class Solution20: Solving {
     self.file = file
   }
 
+  struct Node: CustomStringConvertible, Equatable, Hashable {
+    let index: Int
+    let val: Int
+
+    var description: String {
+      "(\(index): \(val))"
+    }
+
+    static func == (lhs: Self, rhs: Self) -> Bool {
+      lhs.index == rhs.index
+    }
+  }
+
   func solve1() -> String {
-    var list = file.lines.map { Int($0)! }
-    var indices = Array(0..<list.count)
+    var list = file.lines.enumerated().map { Node(index: $0, val: Int($1)!) }
 
     for i in 0..<list.count {
       // print("i", i)
       // print(list)
       // print(indices)
-      let val = list[indices[i]]
+      let (curIndex, node) = list.enumerated().first { $1.index == i }!
       // print("val", val)
-      var newIndex = indices[i] + val
+      var newIndex = curIndex + node.val
       // print("newIndex", newIndex)
-      list.remove(at: indices[i])
-
-      ((i+1)..<indices.count).forEach {
-        indices[$0] -= 1
-      }
+      list.remove(at: curIndex)
 
       let mod = newIndex % list.count
       newIndex = mod >= 0 ? mod : mod + list.count
 
-      list.insert(val, at: newIndex)
-      ((newIndex+1)..<indices.count).forEach {
-        indices[$0] += 1
-      }
+      list.insert(node, at: newIndex)
     }
 
     // print(list)
     // print(indices)
 
-    let index0 = list.firstIndex(of: 0)!
+    let index0 = list.firstIndex { $0.val == 0 }!
     // print(index0)
     let s = [1000, 2000, 3000].map {
       print(list[(index0 + $0) % list.count])
-      return list[(index0 + $0) % list.count]
+      return list[(index0 + $0) % list.count].val
     }.sum()
 
     return s.description
   }
 
   func solve2() -> String {
-    return file.filename
+    var list = file.lines.enumerated().map { Node(index: $0, val: Int($1)! * 811_589_153) }
+
+    for v in 0..<10 {
+      print(v)
+      for i in 0..<list.count {
+        // print("i", i)
+        // print(list)
+        // print(indices)
+        let (curIndex, node) = list.enumerated().first { $1.index == i }!
+        // print("val", val)
+        var newIndex = curIndex + node.val
+        // print("newIndex", newIndex)
+        list.remove(at: curIndex)
+
+        let mod = newIndex % list.count
+        newIndex = mod >= 0 ? mod : mod + list.count
+
+        list.insert(node, at: newIndex)
+      }
+    }
+
+    // print(list)
+    // print(indices)
+
+    let index0 = list.firstIndex { $0.val == 0 }!
+    // print(index0)
+    let s = [1000, 2000, 3000].map {
+      print(list[(index0 + $0) % list.count])
+      return list[(index0 + $0) % list.count].val
+    }.sum()
+
+    return s.description
   }
 }
 
