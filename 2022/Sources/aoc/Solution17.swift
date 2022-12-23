f770e78e22c911f9ef24559f0821af6ebd459c3d
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2022-12-23 12:50:15 +0100

    2022 day 17

--- 2022/Sources/aoc/Solution17.swift
+++ 2022/Sources/aoc/Solution17.swift
@@ -49,7 +49,7 @@ class Solution17: Solving {
     let gusts = file.charsByLine[0]
     let rocks = Rock.allCases
 
-    var grid: [Int: [Int: Character]] = [:]
+    var grid: LazyGrid = [:]
     grid[0] = (0..<7).reduce(into: [:]) {
       $0[$1] = "-"
     }
@@ -85,16 +85,11 @@ class Solution17: Solving {
         rockInd = (rockInd + 1)%Rock.allCases.count
         rockY = grid.highestPoint + 4
         rockX = 2
-        // print(rocksFallen, "\(gust)")
-        // print(grid.prettyDescriptionAdding(rocks[rockInd], x: rockX, y: rockY))
       } else {
         // fall
         rockY -= 1
       }
-      // print(gustInd, "\(gust)")
-      // print(grid.prettyDescriptionAdding(rocks[rockInd], x: rockX, y: rockY))
     }
-    print(grid.topHash)
     return grid.highestPoint.description
   }
 
@@ -113,69 +108,49 @@ class Solution17: Solving {
     var rocksFallen = 0
     var gustInd = 0
 
-    var loopDetection: [Int: (Int, Int)] = [:]
-    var loopDet2: [Int: [([Int], Int, Int)]] = [:]
+    var loopDetection: [Int: [(loop: [Int], rockX: Int, relRockY: Int, rocksFallen: Int, finalHeight: Int, grid: LazyGrid)]] = [:]
+    var curLoop: [Int] = []
+    var detectLoops = true
 
-    var curLoop2: [Int] = []
-    var fallenLast = 0
-    var heightLast = 0
-
-    var limit = 1_000_000_000_000
+    let limit = 1_000_000_000_000
 
     while rocksFallen < limit {
       let gust = gusts[gustInd]
       gustInd = (gustInd + 1)%gusts.count
 
-      if rocksFallen%1_000_000 == 0 {
-        print(rocksFallen)
-      }
-
-      // print(gustInd, rockInd)
-
-      if gustInd == 0 {
-        if let prevLoops = loopDet2[rockInd] {
-          if let loop = prevLoops.first(where: { $0.0 == curLoop2 }) {
-            print("loop found!", loop)
-            print("current:", curLoop2, rocksFallen, grid.highestPoint)
-            let startOffset = loop.1
+      if detectLoops, gustInd == 0 {
+        if let prevLoops = loopDetection[rockInd] {
+          if let loop = prevLoops.first(where: { $0.loop == curLoop && $0.rockX == rockX && $0.relRockY == rockY - grid.highestPoint }) {
+            print("loop found!", loop.loop.count, loop.rockX, loop.relRockY, loop.rocksFallen, loop.finalHeight)
+            print("current:", curLoop.count, rockX, rockY - grid.highestPoint, rocksFallen, grid.highestPoint)
+            // print(loop.grid.prettyDescriptionAdding(rocks[rockInd], x: loop.rockX, y: loop.finalHeight + loop.relRockY))
+            let startOffset = loop.rocksFallen
             let cycleLength = rocksFallen - startOffset
             let loopCount = (limit-startOffset)/cycleLength
-            let leftover = (limit-startOffset)%cycleLength
-            let startHeight = loop.2
-            let loopHeight = grid.highestPoint - loop.2
-            let endLoopheight = startHeight + loopCount * loopHeight
+            let leftover = ((limit-startOffset)%cycleLength)
+            let startHeight = loop.finalHeight
+            let loopHeight = grid.highestPoint - loop.finalHeight
+            let endLoopHeight = startHeight + loopCount * loopHeight
 
-            print("startOffset:", startOffset, "cycle:", cycleLength, "loops until end", loopCount, "leftover", leftover, "end loop height", endLoopheight)
-            limit = rocksFallen + leftover
-            continue
-            // TODO
-            // break
+            print("startOffset:", startOffset, "cycle:", cycleLength, "loops until end", loopCount, "leftover", leftover, "end loop height", endLoopHeight)
+            // print(grid.prettyDescriptionAdding(rocks[rockInd], x: rockX, y: rockY))
+
+            // bootstrap to last few rocks
+            rocksFallen = limit - leftover
+            let gridOffset = endLoopHeight - grid.highestPoint
+            var newGrid: LazyGrid = [:]
+            for (k, v) in grid {
+              newGrid[k+gridOffset] = v
+            }
+
+            grid = newGrid
+            detectLoops = false
           }
         }
-        loopDet2[rockInd, default: []].append((curLoop2, rocksFallen, grid.highestPoint))
-        curLoop2 = [rockInd]
+        loopDetection[rockInd, default: []].append((loop: curLoop, rockX: rockX, relRockY: rockY - grid.highestPoint, rocksFallen: rocksFallen, finalHeight: grid.highestPoint, grid: grid))
+        curLoop = [rockInd]
       } else {
-        curLoop2.append(rockInd)
-      }
-
-
-      if gustInd == 0 && rockInd == 0 {
-        print("fallenSinceLast", rocksFallen - fallenLast, "heightSinceLast", grid.highestPoint - heightLast)
-
-        fallenLast = rocksFallen
-        heightLast = grid.highestPoint
-      }
-
-      if gustInd == 0 && rockInd == 0 && rockY == grid.highestPoint + 4 {
-        // Try to find loop
-        if let prevFallen = loopDetection[grid.topHash] {
-          // print("loop found")
-          print(prevFallen.0, rocksFallen, prevFallen.1, grid.highestPoint)
-          break
-        } else {
-          print("No loop found, rocky", rockY, "highest+4", grid.highestPoint + 4)
-          loopDetection[grid.topHash] = (rocksFallen, grid.highestPoint)
-        }
+        curLoop.append(rockInd)
       }
 
       let rock = rocks[rockInd]
@@ -201,17 +176,14 @@ class Solution17: Solving {
         rockInd = (rockInd + 1)%Rock.allCases.count
         rockY = grid.highestPoint + 4
         rockX = 2
-        // print(rocksFallen, "\(gust)")
-        // print(grid.prettyDescriptionAdding(rocks[rockInd], x: rockX, y: rockY))
       } else {
         // fall
         rockY -= 1
       }
-      // print(gustInd, "\(gust)")
-      // print(grid.prettyDescriptionAdding(rocks[rockInd], x: rockX, y: rockY))
     }
 
-    return grid.highestPoint.description
+    // print(grid.prettyDescriptionAdding(rocks[rockInd], x: rockX, y: rockY))
+    return (grid.highestPoint + 6).description
   }
 }
 
@@ -224,10 +196,11 @@ extension LazyGrid {
   }
 
   var prettyDescriptionInvY: String {
-    (0...highestPoint).map { yInv in
-      let y = highestPoint - yInv
+    let lowestPoint = keys.min()!
+    return (lowestPoint...highestPoint).map { yInv in
+      let y = (highestPoint - yInv) + lowestPoint
 
-      return "|" + String((0...self[0]!.keys.max()!).map { x in
+      return "\(y)|" + String((0..<7).map { x in
         return self[y]?[x] ?? "."
       }) + "|"
     }.joined(separator: "\n")
@@ -238,10 +211,8 @@ extension LazyGrid {
   }
 
   func hits(_ rock: Rock, x: Int, y: Int) -> Bool {
-    // print("gettingMax")
     guard y > 0 else { return true }// hits bottom
     guard x >= 0, x + rock.width <= 7 else { return true } // hits edges
-    // print("GotMax")
 
     return !rock.gridShape.lazy.map {
       point in Point2D(x: x + point.x, y: y + point.y)
@@ -258,39 +229,11 @@ extension LazyGrid {
     }
   }
 
-  var topHash: Int {
-    let top = highestPoint
-
-    var value = 0
-
-    // print((0...4).map { yInv in
-    //   let y = top - yInv
-
-    //   return "|" + String((0...self[0]!.keys.max()!).map { x in
-    //     return self[y]?[x] ?? "."
-    //   }) + "|"
-    // }.joined(separator: "\n"))
-
-    for (i, y) in ((top-4)...top).enumerated() {
-      let row = self[y]!
-      var val = 0
-      for x in row.keys {
-        val += 1 << x
-        // print("x", x, "adding", 1 << x, "val", val)
-      }
-
-      value += val * Int(pow(Double(10), Double(i)))
-      // print("y", y, "adding", val * Int(pow(Double(10), Double(i))), "value", value)
-    }
-
-    return value
-  }
-
   mutating func clean() {
     let lowestPoint = keys.min()!
 
     guard highestPoint - lowestPoint > 100 else { return }
-    // print("cleaning")
+
     for i in lowestPoint...(lowestPoint + 50) {
       self[i] = nil
     }
